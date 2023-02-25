8dd9c9739dfc0d18230448103d82a9719fbc0fb5
snowflakeuser92 <126303388+snowflakeuser92@users.noreply.github.com>
2023-02-25 17:53:16 +1300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -46,7 +46,7 @@ try:
     
 except URLError as e:
   streamlit.error()
-  
+
 
 
 # function 2
@@ -59,14 +59,23 @@ if streamlit.button('Get Fruit Load List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   my_data_rows = get_fruit_load_list()
   streamlit.dataframe(my_data_rows)
-
+  my_cnx.close()
+  
+# add fruit
+def insert_row_snowflake(new_fruit):
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("insert into fruit_load_list values ('"+new_fruit+"')")
+    return "Thanks for adding " + new_fruit
+  
+add_my_fruit = streamlit.text_input('what fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+  my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+  back_from_function = insert_row_snowflake(add_my_fruit)
+  streamlit.text(back_from_function)
+  my_cnx.close()
+  
 streamlit.stop()  
-my_cur = my_cnx.cursor()
-my_cur.execute("INSERT INTO fruit_load_list(fruit_name) VALUES ('test')")
-my_cur.execute("SELECT * FROM fruit_load_list")
-my_data_rows = my_cur.fetchall()
-streamlit.text("The fruit load list contains:")
-streamlit.dataframe(my_data_rows)
+
 
 
 
