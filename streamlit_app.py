1701140eb28afbf2a5e2cd13e6764f7bab906519
snowflakeuser92 <126303388+snowflakeuser92@users.noreply.github.com>
2023-02-25 15:25:47 +1300

    Update streamlit_app.py
    added multi-select option

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,10 @@ streamlit.text('🥑🍞 Avocado Toast')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlist.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
+
+
+
+
